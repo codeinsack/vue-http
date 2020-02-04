d24eceab2445bc277f622ebca6a0cfeb5baa0cab
codeinsack <codeinsack@gmail.com>
2020-02-04 18:15:33 +0300

    add first request

--- src/App.vue
+++ src/App.vue
@@ -29,7 +29,12 @@
     },
     methods: {
       submit() {
-        console.log(this.user)
+        this.$http.post('https://vue-http-caaac.firebaseio.com/data.json', this.user)
+        .then(response => {
+          console.log('response', response)
+        }, error => {
+          console.log('error', error)
+        })
       }
     }
   }
